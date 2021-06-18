115a4622dda5aa0e771dfda3034e335e2d7ec3e5
garammasala29 <kmasa1569@gmail.com>
2021-06-19 01:01:54 +0900

    Refactoring

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,8 +4,7 @@
 require 'optparse'
 require 'etc'
 
-lists = Dir.glob('*').sort
-params = ARGV.getopts('a', 'l', 'r')
+params = ARGV.getopts('alr')
 
 def file_type(ftype)
   {
@@ -28,30 +27,34 @@ def permission(mode)
   }[mode]
 end
 
+COLUMN_NUMBER = 3
+
 # ls -a
-lists = Dir.glob('*', File::FNM_DOTMATCH).sort if params['a']
+lists = params['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
 
 # ls -r
 lists.reverse! if params['r']
 
 # ls -l
+
 if params['l']
   total = lists.sum { |list| File.stat(list).blocks }
   puts "total #{total}"
   lists.each do |list|
     file = File.stat(list)
-    mode = file.mode.to_s(8)[-3..]
+    mode = file.mode.to_s(8)[-3..-1]
     file_mode = file_type(file.ftype) + permission(mode[0]) + permission(mode[1]) + permission(mode[2])
     link = file.nlink
     owner = Etc.getpwuid(file.uid).name
     group = Etc.getgrgid(file.gid).name
     bytes = file.size
-    time = file.mtime.strftime('%m %d %R')
+    time = file.mtime.strftime('%_m %d %R')
+    blocks = file.blocks
     puts "#{file_mode} #{link.to_s.rjust(2)} #{owner}  #{group} #{bytes.to_s.rjust(5)} #{time} #{list}"
   end
 else
-  column = lists.size / 3
-  column += 1 unless (lists.size % 3).zero?
+  column = lists.size / COLUMN_NUMBER
+  column += 1 unless (lists.size % COLUMN_NUMBER).zero?
   lists << nil until (lists.size % column).zero?
   divide_lists = lists.each_slice(column).to_a.transpose
   divide_lists.each do |divide_list|
