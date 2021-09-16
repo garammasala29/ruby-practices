b5f30b8b230a33913e2daa51acc5c7be0a998a2b
garammasala29 <kmasa1569@gmail.com>
2021-09-16 21:53:55 +0900

    add frame class

---
+++ 07.bowling_object/lib/frame.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative './shot'
+
+class Frame
+  attr_reader :first_shot, :second_shot, :third_shot
+
+  def initialize(*shots)
+    @first_shot = Shot.new(shots[0])
+    @second_shot = Shot.new(shots[1])
+    @third_shot = Shot.new(shots[2])
+  end
+
+  def frames_score
+    [@first_shot.shot_score, @second_shot.shot_score, @third_shot.shot_score].sum
+  end
+
+  def strike?
+    @first_shot.shot_score == 10
+  end
+
+  def spare?
+    !strike? && [@first_shot.shot_score, @second_shot.shot_score].sum == 10
+  end
+
+  def shots_to_frames
+    @frames = @shots.first(9).map do
+      @shots[0] == 10 ? @shots.shift(1) : @shots.shift(2)
+    end
+    @frames << @shots
+  end
+end
