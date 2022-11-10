e234531c74e43f32445396d12a82e9f0eaf1902d
garammasala29 <kmasa1569@gmail.com>
2022-11-10 17:40:51 +0900

    wc_rowクラスを作成

--- 09.wc_object/lib/wc.rb
+++ 09.wc_object/lib/wc.rb
@@ -1,39 +1,37 @@
 # frozen_string_literal: true
 
+require_relative '../lib/wc_row'
 require 'pathname'
 require 'debug'
 
 class Wc
   def self.run(text: '', file_names: [], line_only: false)
-    texts = []
+    wc_rows = []
     if file_names.empty?
-      texts << [text, '']
+      wc_rows << WcRow.new(text)
     else
       file_names.each do |file_name|
         pathname = Pathname(file_name)
         text = pathname.read
-        texts << [text, file_name]
+        wc_rows << WcRow.new(text, file_name)
       end
     end
     lines = []
     line_count_sum = 0
     word_count_sum = 0
     byte_count_sum = 0
-    texts.each do |text, file_name|
-      line_count = text.lines.count
-      word_count = text.split.size
-      byte_count = text.size
-      line_count_sum += line_count
-      word_count_sum += word_count
-      byte_count_sum += byte_count
+    wc_rows.each do |wc_row, file_name|
+      line_count_sum += wc_row.line_count
+      word_count_sum += wc_row.word_count
+      byte_count_sum += wc_row.byte_count
       line = if line_only
-               "#{line_count.to_s.rjust(8)} #{file_name}".rstrip
+               "#{wc_row.line_count.to_s.rjust(8)} #{wc_row.file_name}".rstrip
              else
-               "#{line_count.to_s.rjust(8)}#{word_count.to_s.rjust(8)}#{byte_count.to_s.rjust(8)} #{file_name}".rstrip
+               "#{wc_row.line_count.to_s.rjust(8)}#{wc_row.word_count.to_s.rjust(8)}#{wc_row.byte_count.to_s.rjust(8)} #{wc_row.file_name}".rstrip
              end
       lines << line
     end
-    if texts.size > 1
+    if wc_rows.size > 1
       line = if line_only
                "#{line_count_sum.to_s.rjust(8)} total"
              else
---
+++ 09.wc_object/lib/wc_row.rb
@@ -0,0 +1,21 @@
+# 1行当たりの必要なデータ
+class WcRow
+  attr_reader :file_name
+
+  def initialize(text, file_name = '')
+    @text = text
+    @file_name = file_name
+  end
+
+  def line_count
+    @text.lines.count
+  end
+
+  def word_count
+    @text.split.size
+  end
+
+  def byte_count
+    @text.size
+  end
+end
